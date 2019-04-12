d2ffcea6f0bb3b8758bf5e05a79c85f12985ba61
Adebayo ANJOLAOLUWA <anjeeade@gmail.com>
2019-04-12 19:32:40 +0100

    implemented responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+function hamburger() {
+    let z = document.getElementById("ham");
+    if (z.className === "center") {
+        z.className += "responsive";
+    } else {
+        z.className === "center";
+    }
+}
